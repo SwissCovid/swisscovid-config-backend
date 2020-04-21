03e55d7fdcdbedb61f321151c6554f5aabef9db8
Felix Haller <haller@ubique.ch>
2020-04-21 14:45:55 +0200

    PP-116 added config endpoint and mock response

--- dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/controller/DPPPTConfigController.java
+++ dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/controller/DPPPTConfigController.java
@@ -1,10 +1,13 @@
 package org.dpppt.backend.sdk.config.ws.controller;
 
+import org.dpppt.backend.sdk.config.ws.model.ConfigResponse;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,4 +29,17 @@ public class DPPPTConfigController {
 		return "Hello from DP3T Config WS";
 	}
 
+	@CrossOrigin(origins = {"https://editor.swagger.io"})
+	@GetMapping(value = "/config")
+	public @ResponseBody
+	ResponseEntity<ConfigResponse> getConfig() {
+		return new ResponseEntity<>(getMockConfigResponse(), HttpStatus.OK);
+	}
+
+	private ConfigResponse getMockConfigResponse() {
+		ConfigResponse configResponse = new ConfigResponse();
+		configResponse.setAppVersion("1.0");
+		configResponse.setOsVersion("ios-12");
+		return configResponse;
+	}
 }
---
+++ dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/model/ConfigResponse.java
@@ -0,0 +1,22 @@
+package org.dpppt.backend.sdk.config.ws.model;
+
+public class ConfigResponse {
+	private String osVersion;
+	private String appVersion;
+
+	public String getOsVersion() {
+		return osVersion;
+	}
+
+	public void setOsVersion(String osVersion) {
+		this.osVersion = osVersion;
+	}
+
+	public String getAppVersion() {
+		return appVersion;
+	}
+
+	public void setAppVersion(String appVersion) {
+		this.appVersion = appVersion;
+	}
+}
