29174727cb5b2af8ca7e28421500868a94802ade
martinalig <martinalig0@gmail.com>
2020-04-22 07:30:43 +0200

    Add response. Fix get params to lowercase.

--- dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/controller/DPPPTConfigController.java
+++ dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/controller/DPPPTConfigController.java
@@ -1,36 +1,32 @@
 package org.dpppt.backend.sdk.config.ws.controller;
 
 import org.dpppt.backend.sdk.config.ws.model.ConfigResponse;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/v1")
 public class DPPPTConfigController {
 
-	private static final DateTimeFormatter DAY_DATE_FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd")
-			.withZone(DateTimeZone.UTC);
 	private static final Logger logger = LoggerFactory.getLogger(DPPPTConfigController.class);
 
-	@CrossOrigin(origins = {"https://editor.swagger.io"})
+	@CrossOrigin(origins = { "https://editor.swagger.io" })
 	@GetMapping(value = "")
-	public @ResponseBody
-	String hello() {
+	public @ResponseBody String hello() {
 		return "Hello from DP3T Config WS";
 	}
 
-	@CrossOrigin(origins = {"https://editor.swagger.io"})
+	@CrossOrigin(origins = { "https://editor.swagger.io" })
 	@GetMapping(value = "/config")
-	public @ResponseBody
-	ResponseEntity<ConfigResponse> getConfig(@RequestParam(required = true) String appVersion,
-											 @RequestParam(required = true) String osVersion) {
-		return new ResponseEntity<>(HttpStatus.OK);
+	public @ResponseBody ResponseEntity<ConfigResponse> getConfig(@RequestParam(required = true) String appversion,
+			@RequestParam(required = true) String osversion) {
+		return ResponseEntity.ok(new ConfigResponse());
 	}
 }
--- dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/model/ConfigResponse.java
+++ dpppt-config-backend/src/main/java/org/dpppt/backend/sdk/config/ws/model/ConfigResponse.java
@@ -1,5 +1,15 @@
 package org.dpppt.backend.sdk.config.ws.model;
 
 public class ConfigResponse {
-	// TODO
+
+	private boolean forceUpdate = false;
+
+	public boolean isForceUpdate() {
+		return forceUpdate;
+	}
+
+	public void setForceUpdate(boolean forceUpdate) {
+		this.forceUpdate = forceUpdate;
+	}
+
 }
