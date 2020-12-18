53f805ddeab0a985aa50a02eb4e73323866095db
alig <alig@ubique.ch>
2020-12-18 07:27:41 +0100

    Refactor mocking
    Enable mock responses for forceupdate and infobox through profiles, by overriding the controller with the mocked responses. This is easier to maintain and we do not need to create mock/test releases each time.

--- dpppt-config-backend/src/main/java/org/dpppt/switzerland/backend/sdk/config/ws/controller/GaenConfigController.java
+++ dpppt-config-backend/src/main/java/org/dpppt/switzerland/backend/sdk/config/ws/controller/GaenConfigController.java
@@ -12,15 +12,18 @@ package org.dpppt.switzerland.backend.sdk.config.ws.controller;
 
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
 
 import org.dpppt.switzerland.backend.sdk.config.ws.helper.IOS136InfoBoxHelper;
+import org.dpppt.switzerland.backend.sdk.config.ws.helper.MockHelper;
 import org.dpppt.switzerland.backend.sdk.config.ws.helper.TestLocationHelper;
-import org.dpppt.switzerland.backend.sdk.config.ws.model.*;
+import org.dpppt.switzerland.backend.sdk.config.ws.model.ConfigResponse;
+import org.dpppt.switzerland.backend.sdk.config.ws.model.FaqEntry;
+import org.dpppt.switzerland.backend.sdk.config.ws.model.InfoBox;
+import org.dpppt.switzerland.backend.sdk.config.ws.model.InfoBoxCollection;
+import org.dpppt.switzerland.backend.sdk.config.ws.model.WhatToDoPositiveTestTexts;
+import org.dpppt.switzerland.backend.sdk.config.ws.model.WhatToDoPositiveTestTextsCollection;
 import org.dpppt.switzerland.backend.sdk.config.ws.poeditor.Messages;
 import org.dpppt.switzerland.backend.sdk.config.ws.semver.Version;
 import org.slf4j.Logger;
@@ -33,6 +36,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import ch.ubique.openapi.docannotations.Documentation;
 
 /**
@@ -91,7 +95,7 @@ public class GaenConfigController {
 
 	private static final Logger logger = LoggerFactory.getLogger(GaenConfigController.class);
 
-	private static Messages messages;
+	private Messages messages;
 
 	public GaenConfigController(Messages messages) {
 		this.messages = messages;
@@ -170,7 +174,7 @@ public class GaenConfigController {
 			@Documentation(description = "Version of the App installed", example = "ios-1.0.7") @RequestParam String appversion,
 			@Documentation(description = "Version of the OS", example = "ios13.6") @RequestParam String osversion,
 			@Documentation(description = "Build number of the app", example = "ios-200619.2333.175") @RequestParam String buildnr) {
-		ConfigResponse body = mockConfigResponseWithInfoBox();
+		ConfigResponse body = MockHelper.mockConfigResponseWithInfoBox(true);
 		return ResponseEntity.ok(body);
 	}
 
@@ -391,88 +395,6 @@ public class GaenConfigController {
 
 		configResponse.setInfoBox(collection);
 
-		return configResponse;
-	}
-
-	private ConfigResponse mockConfigResponseWithInfoBox() {
-		ConfigResponse configResponse = new ConfigResponse();
-
-		InfoBox infoBoxde = new InfoBox();
-		infoBoxde.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz DE");
-		infoBoxde.setTitle("Hinweis DE");
-		infoBoxde.setUrlTitle("Und ein externer Link DE");
-		infoBoxde.setUrl("https://www.bag.admin.ch/bag/de/home.html");
-		InfoBox infoBoxfr = new InfoBox();
-		infoBoxfr.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz FR");
-		infoBoxfr.setTitle("Hinweis FR");
-		infoBoxfr.setUrlTitle("Und ein externer Link FR");
-		infoBoxfr.setUrl("https://www.bag.admin.ch/bag/fr/home.html");
-		InfoBox infoBoxit = new InfoBox();
-		infoBoxit.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz IT");
-		infoBoxit.setTitle("Hinweis IT");
-		infoBoxit.setUrlTitle("Und ein externer Link IT");
-		infoBoxit.setUrl("https://www.bag.admin.ch/bag/it/home.html");
-		InfoBox infoBoxen = new InfoBox();
-		infoBoxen.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz EN");
-		infoBoxen.setTitle("Hinweis EN");
-		infoBoxen.setUrlTitle("Und ein externer Link EN");
-		infoBoxen.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-		InfoBox infoBoxpt = new InfoBox();
-		infoBoxpt.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz PT");
-		infoBoxpt.setTitle("Hinweis PT");
-		infoBoxpt.setUrlTitle("Und ein externer Link PT");
-		infoBoxpt.setUrl("https://www.bag.admin.ch/bag/pt/home.html");
-		InfoBox infoBoxes = new InfoBox();
-		infoBoxes.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz ES");
-		infoBoxes.setTitle("Hinweis ES");
-		infoBoxes.setUrlTitle("Und ein externer Link ES");
-		infoBoxes.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-		InfoBox infoBoxsq = new InfoBox();
-		infoBoxsq.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz SQ");
-		infoBoxsq.setTitle("Hinweis SQ");
-		infoBoxsq.setUrlTitle("Und ein externer Link SQ");
-		infoBoxsq.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-		InfoBox infoBoxbs = new InfoBox();
-		infoBoxbs.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz BS");
-		infoBoxbs.setTitle("Hinweis BS");
-		infoBoxbs.setUrlTitle("Und ein externer Link BS");
-		infoBoxbs.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-		InfoBox infoBoxhr = new InfoBox();
-		infoBoxhr.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz HR");
-		infoBoxhr.setTitle("Hinweis HR");
-		infoBoxhr.setUrlTitle("Und ein externer Link HR");
-		infoBoxhr.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-		InfoBox infoBoxrm = new InfoBox();
-		infoBoxrm.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz RM");
-		infoBoxrm.setTitle("Hinweis RM");
-		infoBoxrm.setUrlTitle("Und ein externer Link RM");
-		infoBoxrm.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-		InfoBox infoBoxsr = new InfoBox();
-		infoBoxsr.setMsg("Hier steht ein Text. Das kann ein Hinweis sein. Je länger umso mehr Platz SR");
-		infoBoxsr.setTitle("Hinweis SR");
-		infoBoxsr.setUrlTitle("Und ein externer Link SR");
-		infoBoxsr.setUrl("https://www.bag.admin.ch/bag/en/home.html");
-
-		InfoBoxCollection collection = new InfoBoxCollection();
-		collection.setDeInfoBox(infoBoxde);
-		collection.setEnInfoBox(infoBoxen);
-		collection.setFrInfoBox(infoBoxfr);
-		collection.setItInfoBox(infoBoxit);
-		collection.setPtInfoBox(infoBoxpt);
-		collection.setEsInfoBox(infoBoxes);
-		collection.setSqInfoBox(infoBoxsq);
-		collection.setHrInfoBox(infoBoxhr);
-		collection.setBsInfoBox(infoBoxbs);
-		collection.setRmInfoBox(infoBoxrm);
-		collection.setSrInfoBox(infoBoxsr);
-		configResponse.setInfoBox(collection);
-
-		return configResponse;
-	}
-
-	public ConfigResponse mockConfigResponseWithForceUpdate() {
-		ConfigResponse configResponse = new ConfigResponse();
-		configResponse.setForceUpdate(true);
 		return configResponse;
 	}
 
