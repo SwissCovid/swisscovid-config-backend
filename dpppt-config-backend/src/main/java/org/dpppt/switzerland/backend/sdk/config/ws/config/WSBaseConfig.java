7b43f53c6969bde8585e21648a0cc564e345dd19
Martin Alig <alig@ubique.ch>
2021-06-17 10:33:16 +0200

    New Flag for notification.

--- dpppt-config-backend/src/main/java/org/dpppt/switzerland/backend/sdk/config/ws/config/WSBaseConfig.java
+++ dpppt-config-backend/src/main/java/org/dpppt/switzerland/backend/sdk/config/ws/config/WSBaseConfig.java
@@ -62,6 +62,8 @@ public abstract class WSBaseConfig implements SchedulingConfigurer, WebMvcConfig
 
 	@Value("${ws.retentiondays: 21}")
 	int retentionDays;
+	@Value("${ws.checkinupdatenotificationenabled: true}")
+	boolean checkInUpdateNotificationEnabled;
 	
 	@Value("#{${ws.security.headers: {'X-Content-Type-Options':'nosniff', 'X-Frame-Options':'DENY','X-Xss-Protection':'1; mode=block'}}}")
 	Map<String,String> additionalHeaders;
@@ -79,7 +81,7 @@ public abstract class WSBaseConfig implements SchedulingConfigurer, WebMvcConfig
 
 	@Bean
 	public GaenConfigController gaenConfigController(Messages messages) {
-		return new GaenConfigController(messages,  interOpsCountryCodes);
+		return new GaenConfigController(messages,  interOpsCountryCodes, checkInUpdateNotificationEnabled);
 	}
 
 	@Bean
